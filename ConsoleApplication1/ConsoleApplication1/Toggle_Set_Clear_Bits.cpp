6e5373b43e1713de6e2198b6821be3655251ee95
Mona Lisa <105809375+ProgMann228@users.noreply.github.com>
2026-02-05 18:48:39 +0300

    Add new files

---
+++ ConsoleApplication1/ConsoleApplication1/Toggle_Set_Clear_Bits.cpp
@@ -0,0 +1,48 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int toggle_bit(int n, unsigned pos)
+{
+    int mask = 1 << pos;
+
+    return n ^ mask;
+}
+
+int set_bit(int n, unsigned pos)
+{
+    int mask = 1 << pos;
+
+    return n | mask;
+}
+
+int clear_bit(int n, unsigned pos)
+{
+    int mask = ~(1 << pos);
+
+    return n & mask;
+}
+
+bool is_bit_set(int n, unsigned pos)
+{
+    int mask = 1 << pos;
+    bool rez = (n & mask) != 0;
+
+    return rez;
+}
+
+int set_multiple_bits(int n, unsigned mask)
+{
+    return n | mask;
+}
+
+int clear_multiple_bits(int n, unsigned mask)
+{
+    return n & ~mask;
+}
+
+int toggle_multiple_bits(int n, unsigned mask)
+{
+    return n ^ mask;
+}
